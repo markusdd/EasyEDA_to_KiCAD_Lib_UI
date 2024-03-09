90570a018ab720fff563bba4e842b84b6ade69e0
Markus Krause <Krause.Markus@gmx.com>
2024-03-09 21:32:10 +0100

    add logo

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> eframe::Result<()> {
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 // NOE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/cpu.png")[..])
                     .unwrap(),
             ),
         ..Default::default()
