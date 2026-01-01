151bb61994be8efd38fc8947341de7cbd8481643
Markus Krause <Krause.Markus@gmx.com>
2026-01-01 21:15:27 +0100

    start work on 1.3.11, configure linter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all, rust_2018_idioms)]
+#![allow(clippy::collapsible_if)]
 
 mod app;
 pub use app::MyApp;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all, rust_2018_idioms)]
+#![allow(clippy::collapsible_if)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
